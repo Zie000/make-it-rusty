957abfcb5bbc44c701ef8c56fa885ee2ff3c15b6
Zie000 <85907258+Zie000@users.noreply.github.com>
2023-01-29 21:11:48 -0500

    Add additional secret variable parsing

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,14 @@ use dotenv::dotenv;
 fn main() {
     dotenv().ok();
     let clientId: String = std::env::var("REDDIT_CLIENT_ID").expect("There must be a reddit client id.");
+    let clientSecret: String = std::env::var("REDDIT_CLIENT_SECRET").expect("There must be a reddit client secret.")
 
-    let client: Reddit = Reddit::new("windows11:redditScraperTool:v0.0.1 (by /u/Zie84)", "")
+    let accountUsername: String = std::env::var("REDDIT_USERNAME").expect("There must be a username");
+    let accountUsername: String = std::env::var("REDDIT_PASSWORD").expect("REDDIT_PASSWORD");
+
+
+    let client: Reddit = Reddit::new("windows11:redditScraperTool:v0.0.1 (by /u/Zie84)", &clientId, &clientSecret);
+
+    client.username("Zie84").password()
     
 }
