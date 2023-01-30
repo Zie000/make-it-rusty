e840b599f193187326ff534b018080637cb7ec5e
Zie000 <85907258+Zie000@users.noreply.github.com>
2023-01-29 23:00:28 -0500

    Getting user data (or error) and putting it in console
    having auth issues with reddit - receiving unauthorized_client every time

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
-use std::env;
-
-use http::{Request, Response};
-use roux::Reddit;
 use dotenv::dotenv;
+use roux::{me::response::MeData, util::RouxError, Me, Reddit};
+use std::io::{stdout, Write};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
-    let clientId: String = std::env::var("REDDIT_CLIENT_ID").expect("There must be a reddit client id.");
-    let clientSecret: String = std::env::var("REDDIT_CLIENT_SECRET").expect("There must be a reddit client secret.")
+    let client_id: String =
+        std::env::var("REDDIT_CLIENT_ID").expect("There must be a reddit client id.");
+    let client_secret: String =
+        std::env::var("REDDIT_CLIENT_SECRET").expect("There must be a reddit client secret.");
 
-    let accountUsername: String = std::env::var("REDDIT_USERNAME").expect("There must be a username");
-    let accountUsername: String = std::env::var("REDDIT_PASSWORD").expect("REDDIT_PASSWORD");
+    let username: String = std::env::var("REDDIT_USERNAME").expect("There must be a username");
+    let password: String =
+        std::env::var("REDDIT_PASSWORD").expect("There must be a configured password");
 
+    let me = login_to_reddit(&client_id, &client_secret, &username, &password).await;
 
-    let client: Reddit = Reddit::new("windows11:redditScraperTool:v0.0.1 (by /u/Zie84)", &clientId, &clientSecret);
+    if me.is_ok() {
+        let me_data_call = me.unwrap().me().await;
+        let me_data = me_data_call.unwrap();
+        println!("{}", me_data.comment_karma);
+    } else {
+        let error = me.unwrap_err();    
+        println!("{}", error.to_string());
+    }
+}
 
-    client.username("Zie84").password()
-    
+async fn login_to_reddit(
+    client_id: &str,
+    client_secret: &str,
+    username: &str,
+    password: &str,
+) -> Result<Me, RouxError> {
+    let client = Reddit::new(
+        "windows:FormattedPostDisassembler:v0.1.1 (by /u/Embarrassed_Salad918)",
+        client_id,
+        client_secret,  
+    )
+    .username(username)
+    .password(password)
+    .login()
+    .await;
+
+    return client;
 }
